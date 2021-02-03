34d670fb8e671b0548d33596b3b5ab5ee94bbd17
vodyn <travnicek.vojtech@email.cz>
2021-02-03 14:13:04 +0100

    processing all rest data works

--- play_button.py
+++ play_button.py
@@ -1,6 +1,6 @@
 
 
-# import process_detections
+import process_detections
 import pandas as pd
 import thresholding
 import glob
@@ -17,14 +17,33 @@ if not os.path.exists(results_path):
 
 
 env_path = '/media/vojtech/DATADRIVE4/hfo_envelopes/'
-files = glob.glob(env_path + '/*/*/*.h5')
+raw_files = glob.glob(env_path + '/*/*/*.h5')
 
-chunk = 100
-i=0
-while (i+2)*chunk <= len(files):
-    thresh_data = thresholding.det_files(files[i*chunk:(i+1)*100], std_multipl=13)
+thresh_folder = '/home/vojtech/Nextcloud/python/UHFO_Pavel/results_generic/thresh_13_data/'
+
+def threshold_envelopes(files):
+    chunk = 100
+    i=0
+    while (i+2)*chunk <= len(files):
+        thresh_data = thresholding.det_files(files[i*chunk:(i+1)*100], std_multipl=13)
+        thresh_data.to_pickle(results_path + 'thresh_data_' + str(i) + '.pkl')
+        i+=1
+
+    thresh_data = thresholding.det_files(files[i*chunk:len(files)], std_multipl=13)
     thresh_data.to_pickle(results_path + 'thresh_data_' + str(i) + '.pkl')
-    i+=1
 
-thresh_data = thresholding.det_files(files[i*chunk:len(files)], std_multipl=13)
-thresh_data.to_pickle(results_path + 'thresh_data_' + str(i) + '.pkl')
+
+def process_thresh_data(thresh_folder, raw_files):
+
+    files = glob.glob(thresh_folder + '*.pkl')
+    res = pd.DataFrame()
+
+    for file in files:
+        res = res.append(process_detections.proces_thresh(file, raw_files))
+
+    return res
+
+processed_data = process_thresh_data(thresh_folder, raw_files)
+processed_data = process_thresh_data.reset_index(drop=True)
+
+processed_data.to_pickle(results_path + 'processed_data.pkl')
--- process_detections.py
+++ process_detections.py
@@ -17,51 +17,55 @@ if not os.path.exists(results_path):
 env_path = '/media/vojtech/DATADRIVE4/hfo_envelopes/'
 data_path = '/home/vojtech/Nextcloud/python/UHFO_Pavel/results_generic/results_2021-02-02/thresh_df.pkl'
 
-files = glob.glob(env_path + '/*/*/*.h5')
-files_temp = [file.split('/')[-1] for file in files]
+raw_files = glob.glob(env_path + '/*/*/*.h5')
 
 
 
-data = pd.read_pickle(data_path)
+def proces_thresh(thresh_file, raw_files):
 
-#prepare files df
-file_df = pd.DataFrame(data = {'files':data.file.unique()})
-file_df['high_env_freq'] = file_df.files.apply(lambda x: x.split('env')[2].split('.')[0])
-file_df['montage'] = file_df.files.apply(lambda x: x.split('_')[-4])
-file_df.loc[file_df.montage =='var', 'montage'] = 'low_var'
+    data = pd.read_pickle(thresh_file)
+    files_temp = [file.split('/')[-1] for file in raw_files]
 
-stat_df = pd.DataFrame()
-for x, file_row in file_df.iterrows():
+    #prepare files df
+    file_df = pd.DataFrame(data = {'files':data.file.unique()})
+    file_df['high_env_freq'] = file_df.files.apply(lambda x: x.split('env')[2].split('.')[0])
+    file_df['montage'] = file_df.files.apply(lambda x: x.split('_')[-4])
+    file_df.loc[file_df.montage =='var', 'montage'] = 'low_var'
 
-    # find file location
-    file_loc = files[files_temp.index(file_row.files)]
-    # read file info
-    data_row, info, fs = read_sp_hdf(file_loc)
-    ch_names = [info[index][0].decode() for index in np.arange(len(info))]
+    stat_df = pd.DataFrame()
+    for x, file_row in file_df.iterrows():
 
-    # file length in microseconds
-    file_len = len(data_row[0,:])/fs*1e6
-    # load thresholded data
-    data_temp = data[data.file==file_row.files]
-    data_temp = data_temp.reset_index(drop=True)
+        # find file location
+        file_loc = raw_files[files_temp.index(file_row.files)]
+        # read file info
+        data_row, info, fs = read_sp_hdf(file_loc)
+        ch_names = [info[index][0].decode() for index in np.arange(len(info))]
 
-    for multipl in data_temp.multipl.unique():
+        # file length in microseconds
+        file_len = len(data_row[0,:])/fs*1e6
+        # load thresholded data
+        data_temp = data[data.file==file_row.files]
+        data_temp = data_temp.reset_index(drop=True)
 
-
-        #prepare file results df
-        res = pd.DataFrame(data = {'file': [file_row.files]*len(ch_names),
-                                   'high_env_freq':[file_row.high_env_freq]*len(ch_names),
-                                   'montage': [file_row.montage]*len(ch_names), 'multipl': [multipl]*len(ch_names),
-                                   'channel': ch_names, 'ratio[%]':[0]*len(ch_names)})
+        for multipl in data_temp.multipl.unique():
 
 
-        data_temp['leng'] = data_temp.stop_time - data_temp.start_time
-        for channel in res.channel.unique():
-            sum_leng = np.sum(data_temp.loc[data_temp.channel == channel, 'leng'])
-            if sum_leng > 0:
-                res.loc[res.channel == channel, 'ratio[%]'] = sum_leng/file_len*100     #value in percent
+            #prepare file results df
+            res = pd.DataFrame(data = {'file': [file_row.files]*len(ch_names),
+                                       'high_env_freq':[file_row.high_env_freq]*len(ch_names),
+                                       'montage': [file_row.montage]*len(ch_names), 'multipl': [multipl]*len(ch_names),
+                                       'channel': ch_names, 'ratio[%]':[0]*len(ch_names)})
+
+
+            data_temp['leng'] = data_temp.stop_time - data_temp.start_time
+            for channel in res.channel.unique():
+                sum_leng = np.sum(data_temp.loc[data_temp.channel == channel, 'leng'])
+                if sum_leng > 0:
+                    res.loc[res.channel == channel, 'ratio[%]'] = sum_leng/file_len*100     #value in percent
 
 
 
-        print(file_row.files)
-        stat_df = stat_df.append(res)
+            print('Processing ' + file_row.files)
+            stat_df = stat_df.append(res)
+
+    return stat_df
